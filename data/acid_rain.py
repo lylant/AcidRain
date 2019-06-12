abd61fd8e2a642a0e92aa89646de4e9c0b1ed169
Lyequation <pphantasm@gmail.com>
2019-06-13 00:32:41 +1000

    blockCreate

--- data/acid_rain.py
+++ data/acid_rain.py
@@ -1,11 +1,13 @@
-import random
-import os
+import random, os
+from random import randint
+
 
 class GameEngine(object):
 
     def __init__(self):
         # initialize some engine variables
-        pass
+        self.blockNameList = []
+        self.wordList = []
 
     def wordListGenerate(self):
         # set a directory for the resource file
@@ -14,24 +16,50 @@ class GameEngine(object):
         pathAbsDir = os.path.join(pathCurrentDir, pathRelDir)
 
         listFile = open(pathAbsDir, encoding="utf-8")
-        listGenerated = []
+        self.wordList = []
 
         # read line by line with readlines(), 'word' temporarily save the string
-        # then saved string is appended in the list 'wordsList'
+        # then saved string is appended in the list 'wordList'
         for word in listFile.readlines():
-            listGenerated.append(word.strip())
+            self.wordList.append(word.strip())
                 # .strip() will remove any character with passed argument
                 # nothing is passed so whitespace will removed (default)
         
-        return listGenerated
-        
 
-    def wordBlockCreate(self, count):
+    def wordBlockCreate(self):
         pass
+            
 
     def wordBlockRemove(self, word):
         pass
 
 
+class wordBlock(object):
+    
+    def __init__(self):
+        self.name = ''
+        self.alive = False
+        self.coordinate = [0, 0]
+    
+    def blockCreate(self, GameEngine):
+        # pass random name for this block and remove it from the word list
+        wordList = GameEngine.wordList  # bind a word list from the object of GameEngine
+        self.name = wordList.pop(random.randrange(len(wordList)))
+
+        # put this block in play
+        self.coordinate[0] = randint(0, 9)
+        self.alive = True
+
+        # add this block to blockNameList
+        (GameEngine.blockNameList).append(self.name)
+
+
+
+
 test = GameEngine()
 test.wordListGenerate()
+test2 = wordBlock()
+test2.blockCreate(test)
+print(test2.coordinate)
+print(test2.name)
+print(test.blockNameList)
--- main.py
+++ main.py
@@ -2,20 +2,6 @@ from tkinter import Tk
 from data import acid_rain
 
 
-class mainEngine(object):
-
-    def wordsListGenerate(self):
-        listFile = open("resource/words.txt", encoding="utf-8")
-        listGenerated = []
-
-        # read line by line with readlines(), 'word' temporarily save the string
-        # then saved string is appended in the list 'wordsList'
-        for word in listFile.readlines():
-            listGenerated.append(word.strip())
-                # .strip() will remove any character with passed argument
-                # nothing is passed so whitespace will removed (default)
-        
-        return listGenerated
 
 
 # construct a tkinter window
